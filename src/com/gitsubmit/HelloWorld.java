81dfc8078bfb29d3687d2b2b1f81a6bfe5ce9009
xiaoyu <yaoyu_best@163.com>
2016-04-29 16:19:21 -0400

    Update HelloWorld.java

--- src/com/gitsubmit/HelloWorld.java
+++ src/com/gitsubmit/HelloWorld.java
@@ -5,10 +5,11 @@ package com.gitsubmit;
 
 /**
  * @author Yayu Yao
- * @time 2016年4月29日下午2:32:53
+ * @time 2016脛锚4脭脗29脠脮脧脗脦莽2:32:53
  */
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("123xxxxxxxxxxxxxwerwerxxxxxxx");
+		System.out.println("this is master");
 	}
 }
