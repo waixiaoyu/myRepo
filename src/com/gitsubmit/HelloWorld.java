17f90986e4d3fab8ac008e85b26ba4f20fea0273
waixiaoyu <yaoyu_best@163.com>
2016-04-29 14:34:15 -0400

    my commit

---
+++ src/com/gitsubmit/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.gitsubmit;
+
+/**
+ * @author Yayu Yao
+ * @time 2016年4月29日下午2:32:53
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+	}
+}
