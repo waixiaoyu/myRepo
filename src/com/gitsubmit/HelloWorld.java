53aa7308d797ca2f99acc61150a457fed0a4fb92
xiaoyu <yaoyu_best@163.com>
2016-05-02 19:06:10 -0400

    Update HelloWorld.java

--- src/com/gitsubmit/HelloWorld.java
+++ src/com/gitsubmit/HelloWorld.java
@@ -9,8 +9,8 @@ package com.gitsubmit;
  */
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("helloWorld");
-		System.out.println("helloWorld");
-		System.out.println("helloWorld");
+		System.out.println("new");
+		System.out.println("new");
+		System.out.println("new");
 	}
 }
