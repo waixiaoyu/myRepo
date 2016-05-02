f6229442784369c62f95f2ec251ecfe644be62ae
waixiaoyu <yaoyu_best@163.com>
2016-05-02 19:08:03 -0400

    master

--- src/com/gitsubmit/HelloWorld.java
+++ src/com/gitsubmit/HelloWorld.java
@@ -9,8 +9,8 @@ package com.gitsubmit;
  */
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("helloWorld");
-		System.out.println("helloWorld");
-		System.out.println("helloWorld");
+		System.out.println("master");
+		System.out.println("master");
+		System.out.println("master");
 	}
 }
