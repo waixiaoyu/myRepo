da821d056685ae5d49a37fb11d10820790f51353
waixiaoyu <yaoyu_best@163.com>
2016-05-26 10:57:30 +0800

    qwqweqweeqwe

--- src/com/gitsubmit/HelloWorld.java
+++ src/com/gitsubmit/HelloWorld.java
@@ -9,8 +9,9 @@ package com.gitsubmit;
  */
 public class HelloWorld {
 	public static void main(String[] args) {
+		System.out.println("526");
+		System.out.println("526");
 		System.out.println("master");
-		System.out.println("master");
-		System.out.println("master");
+
 	}
 }
