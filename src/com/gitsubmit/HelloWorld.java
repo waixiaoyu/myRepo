20988cebcc055ee4d5ac092678c35e0010d7c8dc
waixiaoyu <yaoyu_best@163.com>
2016-05-26 10:59:22 +0800

    526

--- src/com/gitsubmit/HelloWorld.java
+++ src/com/gitsubmit/HelloWorld.java
@@ -11,7 +11,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("526");
 		System.out.println("526");
-		System.out.println("master");
+		System.out.println("526");
 
 	}
 }
